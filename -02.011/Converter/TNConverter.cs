882c0d8e06447bdb9e577d71fb3ea9d892aee9e5
Asar_kun <asarkrylov76212@gmail.com>
2023-12-17 01:31:15 +0300

    Add Converter for slice big string and begin works with main logic

---
+++ -02.011/Converter/TNConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _02._011.Converter
+{
+    public class TNConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue && stringValue.Length > 10)
+            {
+                return stringValue.Substring(0, 10) + "...";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- -02.011/WINDOWS/Pages/PageMenu.xaml.cs
+++ -02.011/WINDOWS/Pages/PageMenu.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _02._011.Converter;
+using MaterialDesignColors.ColorManipulation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,9 +27,36 @@ namespace _02._011.WINDOWS.Pages
             InitializeComponent();
         }
 
+        private void Button_Back(object sender, RoutedEventArgs e)
+        {
+            Navigation.Navigation.СurrentFrame.Navigate(new PageEntry());
+        }
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            TaskNoteTable.Columns.Clear();
+            TaskNoteTable.Columns.Add(CreateColumn("Created", "CreateDate"));
+            TaskNoteTable.Columns.Add(CreateColumn("ShortName", "ShortName"));
+            TaskNoteTable.Columns.Add(CreateColumn("Tags", "Tags"));
+            TaskNoteTable.Columns.Add(CreateColumn("PlannedDate", "PlannedDate"));
+            TaskNoteTable.Columns.Add(CreateColumn("DaysLeft", "DaysLeft"));
+            TaskNoteTable.Columns.Add(CreateColumn("Completion", "DateСompletion"));
+            TaskNoteTable.Columns.Add(CreateColumn("Type", "Type.Name"));
+        }
 
+        public static DataGridTextColumn CreateColumn(string header, string binding)
+        {
+            DataGridTextColumn column = new()
+            {
+                Header = header,
+                IsReadOnly = true,
+            };
+            Binding textBinding = new(binding)
+            {
+                Converter = new TNConverter()
+            };
+            column.Binding = textBinding;
+            return column;
         }
     }
 }
