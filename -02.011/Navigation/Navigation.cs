5688354f7995c716a05dfc2e15da9e4667f552d9
Asar_kun <asarkrylov76212@gmail.com>
2023-12-17 01:10:38 +0300

    Add class Navigation

---
+++ -02.011/Navigation/Navigation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _02._011.Navigation
+{
+    internal class Navigation //Класс со стат методом для получения и задания текущего фрейма, так как работа через страницы
+    {
+        public static Frame СurrentFrame { get; set; }
+    }
+}
